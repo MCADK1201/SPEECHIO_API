895244d7da08121ca4e9b8b26ab3836b65a44489
MCADK1201 <113371892+MCADK1201@users.noreply.github.com>
2022-12-03 20:33:14 +0530

    Update download.js

--- download.js
+++ download.js
@@ -1,28 +1,28 @@
 async function _mergeAll(n) {
+	let a;
 	if (n.length) {
 		$("#download").children().replaceWith('\n\n            <div class="spinner-border text-info" role="status">\n\n  <span class="sr-only">Loading...</span>\n\n</div>\n\n          '), $("#download").addClass("btn-outline-info").removeClass("btn-outline-info").addClass("disabled"), $(".remove").each((function() {
 			$(this).addClass("disabled")
 		}));
-		const o = new Date;
 		let e = $("#download").html();
-		e.split(/id="blob/g), e = "blob" + e[1], e = e.split(/">/g), console.log(e);
-		let a = new RegExp(/^blob:http/g);
-		void 0 !== e && "" !== e && null !== e ? a.test(e) ? (ajax_download(e, o + ".mp3"), window.URL.revokeObjectURL(e), $("#download").removeClass("btn-outline-secondary").addClass("btn-outline-info").children().replaceWith(`<i class="fas fa-file-arrow-down mr-2">`)) : a.test(e) || _concat("concatAudio", o, n) : _concat("concatAudio", o, n)
+		e = e.split(/id="/g)[1], "download-again" == e ? _download_(a) : a = _concat("concatAudio", o, n), $("#download").removeClass("btn-outline-secondary").addClass("btn-outline-info").removeClass("disabled").children().replaceWith('<i class="fas fa-file-arrow-down mr-2" id="download-again">')
 	}
 }
-async function _concat(n, o, e) {
-	const a = new Crunker.default({ sampleRate: 44100 }),
-		l = await a.fetchAudio(...e),
-		t = await a[n](l),
-		d = await a.export(t, "audio/mp3");
-	let i = await a.download(d.blob, o);
-	$("#download").removeClass("btn-outline-info").removeClass("disabled").addClass("btn-outline-info").children().replaceWith(`<i class="fas fa-file-arrow-down mr-2" id="${i}">`)
+async function _concat(n, o, a) {
+	const e = new Crunker.default({
+			sampleRate: 44100
+		}),
+		d = await e.fetchAudio(...a),
+		l = await e[n](d),
+		t = await e.export(l, "audio/mp3");
+	return _download_(t.blob), t.blob
 }
 
-function ajax_download(n, o) {
-	fetch(n).then((n => n.blob())).then((n => {
-		const e = window.URL.createObjectURL(n),
-			a = document.createElement("a");
-		a.style.display = "none", a.href = e, a.download = o, document.body.appendChild(a), a.click(), a.remove(), window.URL.revokeObjectURL(e)
-	})).catch((() => {}))
+function _download_(n) {
+	n = new Blob(n);
+	let o = new Date;
+	o += ".mp3";
+	const a = window.URL.createObjectURL(n),
+		e = document.createElement("a");
+	e.style.display = "none", e.href = a, e.download = o, document.body.appendChild(e), e.click(), e.remove(), window.URL.revokeObjectURL(a)
 }

bee37cf90dc634dfd655493d3367408b53d06b19
Dev Dk <113371892+MCADK1201@users.noreply.github.com>
2022-12-03 15:41:45 +0000

    .

--- download.js
+++ download.js
@@ -1,28 +1,28 @@
+let a;
 async function _mergeAll(n) {
-	let a;
 	if (n.length) {
-		$("#download").children().replaceWith('\n\n            <div class="spinner-border text-info" role="status">\n\n  <span class="sr-only">Loading...</span>\n\n</div>\n\n          '), $("#download").addClass("btn-outline-info").removeClass("btn-outline-info").addClass("disabled"), $(".remove").each((function() {
+		$("#download").html('\n\n            <div class="spinner-border text-info" role="status">\n\n  <span class="sr-only">Loading...</span>\n\n</div>\n\n          '), $("#download").addClass("btn-outline-secondary").removeClass("btn-outline-info").addClass("disabled"), $(".remove").each((function() {
 			$(this).addClass("disabled")
 		}));
 		let e = $("#download").html();
-		e = e.split(/id="/g)[1], "download-again" == e ? _download_(a) : a = _concat("concatAudio", n), $("#download").removeClass("btn-outline-secondary").addClass("btn-outline-info").removeClass("disabled").children().replaceWith('<i class="fas fa-file-arrow-down mr-2" id="download-again">')
-	}
+		alert(e), e = e.split(/id="/g)[1], "download-again" == e ? (_download_(a), alert("gain")) : a = _concat("concatAudio", n), $("#download").removeClass("btn-outline-secondary").addClass("btn-outline-info").removeClass("disabled").children().replaceWith('<i class="fas fa-file-arrow-down mr-2" id="download-again">')
+	} else $("download").addClass("disabled")
 }
-async function _concat(n, a) {
+async function _concat(n, b) {
 	const e = new Crunker.default({
 			sampleRate: 44100
 		}),
-		d = await e.fetchAudio(...a),
-		l = await e[n](d),
-		t = await e.export(l, "audio/mp3");
-	return _download_(t.blob), t.blob
+		o = await e.fetchAudio(...b),
+		d = await e[n](o),
+		l = await e.export(d, "audio/mp3");
+		_download_(l.blob);
+        a = l.blob
 }
 
 function _download_(n) {
-	n = new Blob(n);
-	let o = new Date;
-	o += ".mp3";
-	const a = window.URL.createObjectURL(n),
-		e = document.createElement("a");
-	e.style.display = "none", e.href = a, e.download = o, document.body.appendChild(e), e.click(), e.remove(), window.URL.revokeObjectURL(a)
+	let z = new Date;
+	z += ".mp3";
+	const e = window.URL.createObjectURL(n),
+		o = document.createElement("a");
+	o.style.display = "none", o.href = e, o.download = z, document.body.appendChild(o), o.click(), o.remove(), window.URL.revokeObjectURL(e)
 }

66ec69dae38d31e9c4069a1ac3b801f5b33f64f7
MCADK1201 <113371892+MCADK1201@users.noreply.github.com>
2022-12-08 20:28:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -95,7 +95,7 @@ function change_voice(t) {
 	let n = $(t).html();
 	n = n.split('src="')[1], n = n.split('.png"')[0];
 	let e = $(t).html();
-	e = e.replace(/src="\w+.\w+"/g, `src="${{karishma:"james",james:"akash",akash:"karishma"}[n]}.png"`), $(t).html(e)
+	e = e.replace(/src="\w+.\w+"/g, `src="${{"karishma":"mohan","james":"karishma","akash":"james","nitika":"akash","mohit":"nitika","devika":"mohit","navneet":"devika","mohan":"navneet"}[n]}.png"`), $(t).html(e)
 }
 
 function default_voice() {
